d966b2173a8d31335f1cd1395b9a3b50a46bb4af
Brandon Wallerson <blwallerson@gmail.com>
2018-03-05 16:42:09 -0500

    Updates 3/5

--- spiders/Scraper.py
+++ spiders/Scraper.py
@@ -6,19 +6,24 @@ import json
 class CompanySpider(scrapy.Spider):
 
     name = "company_spider"
-    start_urls = ['https://www.cs.purdue.edu/corporate/partners/index.html']
+    start_urls = ['http://corporate.northwestern.edu/highlighted-partnerships/index.html']
 
     def parse(self, response):
-        SET_SELECTOR = '.content'
+        SET_SELECTOR = '//*[@id="main-content"]'
 
 
-        for coset2 in response.css(SET_SELECTOR):
-            TABLE_SELECTOR = './/strong/text()'
-
+        for coset2 in response.xpath(SET_SELECTOR):
+            TABLE_SELECTOR = 'strong::text'
             yield{
-                'table': coset2.xpath(TABLE_SELECTOR).extract(),
-                'friend': coset2.xpath('.//ul[2]/li/a/text()').extract(),
-                'advisor': coset2.xpath('.//div/div[2]/ul[1]/li/a/text()').extract(),
+                'corp_spons' : coset2.css(TABLE_SELECTOR).extract(),
+                #'friend_level': coset2.xpath('//table[1]/tbody/tr/td/text()').extract(),
+                #'details': coset2.css('p::text').extract(),
+                #'details': coset2.xpath('.//p/text()').extract(),
+                #'bullets': coset2.xpath('.//ul/li/text()').extract()
+                #'silver_comp_plans': coset2.xpath('.//ul[7]/li/text()').extract(),
+                #'bronze_comp_plans': coset2.xpath('.//p[46]/text()').extract(),
+                # 'mega': coset2.xpath('//*[@id="partners"]/div/div/div[5]/div/div/div/table/tbody/tr/td/div/span/a/text()').extract(),
+                #'kilo': coset2.xpath('//*[@id="partners"]/div/div/div[7]/div/div/div/table/tbody/tr/td/div/span/a/text()').extract(),
              }
 
 
